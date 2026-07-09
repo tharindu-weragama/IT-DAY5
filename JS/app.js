69c730bdf0373408fde941cff4c8356e452001b7
tharindu-weragama <tharindu.weragama98@gmail.com>
2026-07-09 23:41:37 +0530

    feat: add JavaScript and TypeScript files for variable and operator examples; link app.js in index.html

---
+++ JS/app.js
@@ -0,0 +1,82 @@
+
+//Variables
+
+/*console.log("hello java script");
+let name = "Tharindu";
+let named = 10;
+name = "ranil"
+console.log(typeof named)
+console.log(name)
+
+alert("hello icet") */
+
+//Variable types
+
+/*
+
+let name = "saman";
+let age = 18;
+let isAfterAL = false;
+let customers = [];
+let customer = {};
+let salary = 7500.0;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isAfterAL);
+console.log(typeof customer);
+console.log(typeof customers);
+console.log(typeof salary);*/
+
+//Operators
+
+//Arithmatic operators
+
+// + | - | * | /
+
+/*let x = 10;
+let y = 20;
+
+let z = x + y;
+let a = x - y;
+let b = x * y;
+let c = x / y;
+let e = x % y;
+
+console.log(z);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(e);*/
+
+//Comparison operators
+
+/*let x = 10;
+let y = "10";
+
+console.log(x==y);
+
+console.log(x===y);*/
+
+// Logical opeators
+
+// && | || | !
+
+/*if (!true) {
+    console.log("true");
+} else {
+    console.log("false")
+}
+
+if (true || false) {
+    console.log("true");
+} else {
+    console.log("false")
+}
+
+if (true && false) {
+    console.log("true");
+} else {
+    console.log("false")
+}
+*/
---
+++ JS/app.ts
@@ -0,0 +1,4 @@
+{
+    let name: string = "saman";
+    console.log(name);
+}
